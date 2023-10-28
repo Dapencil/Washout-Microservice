2a7c274b9906295739e13d499437ec7799cffda3
disor <disorn.bootso@gmail.com>
2023-10-28 21:19:32 +0700

    Add CORS to webserver

--- Order/server.js
+++ Order/server.js
@@ -1,12 +1,18 @@
 require("dotenv").config({ path: "./config.env" });
 
 const PROTO_PATH = process.env.PROTO_PATH || "./proto/order.proto";
+const NODE_ENV = process.env.NODE_ENV || "DEV";
+const PORT = process.env.PORT || 30046;
+const DATABASE_URL = process.env[`${NODE_ENV}_DATABASE_URL`];
+const IP = processs.env[`${NODE_EVN}_IP`] || "127.0.0.1";
+
 let grpc = require("@grpc/grpc-js");
 let protoLoader = require("@grpc/proto-loader");
 const { v4: uuidv4 } = require("uuid");
 const mongoose = require("mongoose");
+const Order = require("./model/order");
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -14,7 +20,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
-const Order = require("./model/order");
 
 let packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
@@ -84,13 +89,12 @@ server.addService(protoDescriptor.OrderService.service, {
   },
 });
 
-const PORT = process.env.PORT || 30046;
-
 server.bindAsync(
-  `127.0.0.1:${PORT}`,
+  `${IP}:${PORT}`,
   grpc.ServerCredentials.createInsecure(),
   () => {
     server.start();
+    console.log("Starting on", NODE_ENV);
     console.log(`Order Service Started at PORT ${PORT}`);
   }
 );
