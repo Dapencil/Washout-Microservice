fb0eeba881449eaaa9ea678ff197e163f5ff2fb8
Parichaya_sirin@outlook.com <Parichaya_sirin@outlook.com>
2023-11-03 16:07:51 +0700

    Adding login

--- APIGateway/routers/api.js
+++ APIGateway/routers/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authen");
+const axios = require("axios");
 
 const machinesRouter = require("./machines");
 const branchesRouter = require("./branches");
@@ -16,5 +17,18 @@ router.use("/machines", machinesRouter);
 router.use("/branches", branchesRouter);
 router.use("/users", usersRouter);
 router.use("/orders", ordersRouter);
+
+router.post("/login", (req, res) => {
+  axios
+    .post("http://localhost:3003/login", req.body, {})
+    .then(function (response) {
+      console.log(response);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+});
 
 module.exports = router;
